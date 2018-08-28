dde8e8e42cd2770147e406ac85819d9e555c2c11
caozuohua <caozuohua@qq.com>
2018-08-28 16:00:09 +0800

    测试一种多平台(amd64/arm64/...)的脚本的安装方法

--- debian/foo.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the foo package
-#
-0 4	* * *	root	[ -x /usr/bin/foo_maintenance ] && /usr/bin/foo_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(foo):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="foo" command="/usr/bin/foo"
